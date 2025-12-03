9d48575f4cbff37366a11e7f778a31082698d1f8
MumairA404 <umair.m.amir@gmail.com>
2025-12-03 11:14:11 +0100

    test commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.smarttas {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.smarttas to javafx.fxml;
+    exports com.example.smarttas;
+}
