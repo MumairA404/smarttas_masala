955c63f2757a10890fe0ce6a88cfc1c71b01b200
Ilias B <302472872@student.rocmondriaan.nl>
2025-12-12 14:52:56 +0100

    grote aanpassing Ilias

--- src/main/java/com/example/smarttas/Main.java
+++ src/main/java/com/example/smarttas/Main.java
@@ -18,6 +18,7 @@ public class Main {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+
         Application.launch(HelloApplication.class, args);
     }
 }
