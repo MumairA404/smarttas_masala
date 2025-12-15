0d0b696c2f6a020c4f235e567ef08e6a8d440f56
chernorbah120-cell <chernorbah120@gmail.com>
2025-12-15 12:00:37 +0100

    wat aanpassingen en alle error weggewerkt

--- src/main/java/com/example/smarttas/HelloApplication.java
+++ src/main/java/com/example/smarttas/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("loginpagina.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("registreer.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("SmartTas");
         stage.setScene(scene);
---
+++ src/main/java/com/example/smarttas/controllers/RegisterController.java
@@ -0,0 +1,12 @@
+package com.example.smarttas.controllers;
+import javafx.fxml.FXML;
+
+public class RegisterController {
+
+
+    @FXML
+    public void initialize(){
+        System.out.println("RegisterController geladen");
+    }
+
+}
