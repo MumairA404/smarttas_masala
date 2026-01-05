91af8734c2dc606796f79ee3572e7c0202781c48
MumairA404 <umair.m.amir@gmail.com>
2026-01-05 23:33:45 +0100

    homepage en klassen overzicht connectie

--- src/main/java/com/example/smarttas/HelloApplication.java
+++ src/main/java/com/example/smarttas/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home_page.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 730);
         stage.setTitle("SmartTas");
         stage.setScene(scene);
         stage.show();
